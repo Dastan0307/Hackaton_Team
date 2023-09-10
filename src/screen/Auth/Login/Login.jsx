3c3c9a5d2b1e1c2c9e162c9501944b882136e8a1
unknown <jylkychievdastan.0307@gmail.com>
2023-09-10 20:23:10 +0600

    add: login page

--- src/screen/Auth/Login/Login.jsx
+++ src/screen/Auth/Login/Login.jsx
@@ -1,7 +1,44 @@
-
+import GoogleIcon from '@mui/icons-material/Google';
+import logo from '../../../assets/Logo.svg'
+import '../auth.scss'
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+        <div className="container">
+            <div className="register__info">
+                <img src={logo} alt="error :(" className="register__info_logo" />
+                <div className="register__info_mini">
+                    <h2>Жетекчилер<p>Убакыт үнөмдө</p></h2>
+                    <h2 className='register__info_text-long'>Ата-энелерге<p>Бала бакчадан балыны коруп туруу</p></h2>
+                    <h2 className='register__info_text-smoll'>Тарбиячыларга<p>Бардык маалыматтар кол алдында</p></h2>
+                    <hr className='register__info_line' />
+                </div>
+                <div className="register__info_list">
+                    <label ><input type="checkbox" /> Табель</label>
+                    <label ><input type="checkbox" /> Балдар жонундо толук маалымат</label>
+                    <label ><input type="checkbox" /> Онлайн дубал гезити</label>
+                    <label ><input type="checkbox" /> Төлөмдөр</label>
+                    <label ><input type="checkbox" /> Тамактануу</label>
+                    <label ><input type="checkbox" /> Баланын өнүгүшү</label>
+                </div>
+            </div>
+            <div className="login__form">
+                <div className="login__form_container">
+                  <h2>Аккаунтка кирүү</h2>
+                  <label>Электрондук почта</label>
+                  <input type="email" placeholder='user@gmail.com' />
+                  <label className='login__form_label'>Сыр сөз <a href="#">Сыр сөзүңүздү унуттуңузбу?</a></label>
+                  <input type="password" placeholder='********' />
+                  <button className='login__form_btn'>Кирүү</button>
+                  <p>же болбосо</p>
+                  <button className='login__form_btn-google'><GoogleIcon color='primary' style={{ marginRight: 7 }}/>Google аркылуу кирүү</button>
+                  <a href="register">Азырынча аккантуңуз жокпу? Катталуу</a>
+                </div>
+            </div>
+        </div>
+    </>
+  )
 };
 
 export default Login;
